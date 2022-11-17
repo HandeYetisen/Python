ea1238ab091994bc7efd9b5c9cbee823cd925016
Hande Yetişen <hande.yetisen@etiya.com>
2022-11-17 17:20:48 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,54 @@
+print("Merhaba Etiya")
+text = "10"
+print(text)
+number = 10
+print(number)
+#! string = metinsel ifade
+text = "10"
+print(text)
+
+#! integer = tam sayı
+number = 10
+
+#! double,float,decimal = ondalıklı sayı
+
+dnumber = 5.3
+print (dnumber)
+print(number + 5)
+print (number - 5)
+print (number * 5)
+print (number / 5)
+print (number % 3)
+print ( 1== 1)
+print(1 == 2)
+print(1 != 2)
+text = "merhaba etiya"
+print(text.upper())
+print (text.lower())
+print(number%2 == 0)
+print (2 < 2)
+print(2 > 2)
+print(2 >= 2)
+print(2 <= 2)
+text = "Merhaba Etiya"
+print(text.startswith("Me"))
+print(text.endswith("iya"))
+name = "Halit"
+age = "23"
+company = "Kodlamaio"
+#! Halit 23 yaşında Kodlamaio'da çalışıyor.
+print(name + " "+ age + " yaşında " + company + "'de çalışıyor")
+print(f"{name} {age} yaşında {company}'de çalışıyor")
+print(1 == 1)
+print(1 != 2)
+text = "merhaba etiya"
+print(text.upper())
+print(text.lower())
+print(text.startswith("me"))
+print(text.endswith("iya"))
+print(f"{name} {age} yaşında {company}'de çalışıyor")
+print(2 < 1)
+print(50/2 == 25)
+text = "İlk Gün Ödevi"
+print(text)
+print(text)
